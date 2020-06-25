16f5c527cb24bff45adeefd23f6062e99db73da5
bossondehiggs <54042656+bossondehiggs@users.noreply.github.com>
2020-06-25 14:57:11 +0200

    2.1.6 List all requests

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,11 +1,11 @@
 class GigsController < ApplicationController
-  
+
   protect_from_forgery except: [:upload_photo]
   before_action :authenticate_user!, except: [:show]
   before_action :set_gig, except: [:new, :create]
   before_action :is_authorised, only: [:edit, :update, :upload_photo, :delete_photo]
   before_action :set_step, only: [:update, :edit]
-  
+
   def new
     @gig = current_user.gigs.build
     @categories = Category.all
@@ -16,7 +16,7 @@ class GigsController < ApplicationController
 
     if @gig.save
       @gig.pricings.create(Pricing.pricing_types.values.map{ |x| {pricing_type: x} })
-      redirect_to edit_gig_path(@gig), notice: "Guardando..."
+      redirect_to edit_gig_path(@gig), notice: "Guardar..."
     else
       redirect_to request.referrer, flash: { error: @gig.errors.full_messages }
     end
@@ -26,71 +26,59 @@ class GigsController < ApplicationController
     @categories = Category.all
   end
 
-
   def update
 
-  
-
     if @step == 2
-        gig_params[:pricings_attributes].each do |index, pricing|
-          if @gig.has_single_pricing && pricing[:pricing_type] != Pricing.pricing_types.key(0)
-            next;
-          else
-            if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
-              return redirect_to request.referrer, flash: {error: "Precio erroneo"}
-             end
+      gig_params[:pricings_attributes].each do |index, pricing|
+        if @gig.has_single_pricing && pricing[:pricing_type] != Pricing.pricing_types.key(0)
+          next;
+        else
+          if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
+            return redirect_to request.referrer, flash: {error: "Precio no válido"}
           end
         end
       end
+    end
 
-   
+    if @step == 3 && gig_params[:description].blank?
+      return redirect_to request.referrer, flash: {error: "La descripción no puede estar en blanco"}
+    end
 
-      if @step == 3 && gig_params[:description].blank?
-        return redirect_to request.referrer, flash: {error: "La descripción no puede estar en blanco"}
+    if @step == 4 && @gig.photos.blank?
+      return redirect_to request.referrer, flash: {error: "Necesitas subir al menos una foto"}
+    end
 
-      end
-
-      if @step == 4 && @gig.photos.blank?
-        return redirect_to request.referrer, flash: {error: "Debes añadir alguna foto a la galería"}
-
-      end
-
-      if @step == 5 
-        @gig.pricings.each do |pricing|
-          if @gig.has_single_pricing && !pricing.basico?
-            next;
-          else
-            if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
-              return redirect_to edit_gig_path(@gig, step: 2), flash: {error: "Precio no válido"}
+    if @step == 5
+      @gig.pricings.each do |pricing|
+        if @gig.has_single_pricing && !pricing.basico?
+          next;
+        else
+          if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
+            return redirect_to edit_gig_path(@gig, step: 2), flash: {error: "Invalid pricing"}
           end
         end
       end
 
       if @gig.description.blank?
-        return redirect_to edit_gig_path(@gig, step:3), flash: {error: "La descripción no puede estar en blanco"}
+        return redirect_to edit_gig_path(@gig, step: 3), flash: {error: "La descripción no puede estar en blanco"}
       elsif @gig.photos.blank?
-        return redirect_to edit_gig_path(@gig, step:4), flash: {error: "Debes añadir alguna foto a la galería"}
-        
-
+        return redirect_to edit_gig_path(@gig, step: 4), flash: {error: "Necesitas subir al menos una foto"}
       end
-      end
-
-      if @gig.update(gig_params)
-        flash[:notice] = "Guardado..."
-      else
-        return redirect_to request.referrer, flash: {error: @gig.errors.full_messages}
-      
     end
 
-      if @step < 5
-        redirect_to edit_gig_path(@gig, step: @step + 1)
-      else
-        redirect_to dashboard_path
-      end
+    if @gig.update(gig_params)
+      flash[:notice] = "Guardado..."
+    else
+      return redirect_to request.referrer, flash: {error: @gig.errors.full_messages}
+    end
 
-      end
+    if @step < 5
+      redirect_to edit_gig_path(@gig, step: @step + 1)
+    else
+      redirect_to dashboard_path
+    end
 
-
+  end
 
   def show
     @categories = Category.all
@@ -105,30 +93,28 @@ class GigsController < ApplicationController
     @image = ActiveStorage::Attachment.find(params[:photo_id])
     @image.purge
     redirect_to edit_gig_path(@gig, step: 4)
-    
   end
 
   private
 
   def set_step
     @step = params[:step].to_i > 0 ? params[:step].to_i : 1
-    if @step >5
+    if @step > 5
       @step = 5
+    end
   end
-end
 
   def set_gig
     @gig = Gig.find(params[:id])
   end
 
   def is_authorised
-    redirect_to root_path, alert: "No puedes hacer eso" unless current_user.id == @gig.user_id
+    redirect_to root_path, alert: "No tienes permiso.." unless current_user.id == @gig.user_id
   end
 
   def gig_params
-    params.require(:gig).permit(:title, :video, :description, :active, :category_id, :has_single_pricing,
-                                  pricings_attributes: [:id, :title, :description, :delivery_time, :price, :pricing_type])
-
+    params.require(:gig).permit(:title, :video, :description, :active, :category_id, :has_single_pricing, 
+                                pricings_attributes: [:id, :title, :description, :delivery_time, :price, :pricing_type])
   end
-
 end
+
